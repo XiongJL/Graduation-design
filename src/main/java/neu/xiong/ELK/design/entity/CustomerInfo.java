d622ee614f0ccd1d050215eca91d32f1dd127eaf
Tee <511593318@qq.com>
2018-11-06 15:07:50 +0800

    增加了一个实例

--- src/main/java/neu/xiong/ELK/design/entity/CustomerInfo.java
+++ src/main/java/neu/xiong/ELK/design/entity/CustomerInfo.java
@@ -1,11 +1,25 @@
 package neu.xiong.ELK.design.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ *  infoid :  用户详细信息ID
+ *  username : 用户名称,与customer表关联副键
+ *  address: 收货地址
+ *  avatar : 头像地址
+ *  introduction:  自我介绍
+ * @author Administrator
+ *
+ */
 @Entity
 @Table(name="customerInfo")
 public class CustomerInfo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int infoid;
 	
 	private String username;
@@ -15,6 +29,52 @@ public class CustomerInfo {
 	private String avatar;
 	
 	private String introduction;
+
+	public int getInfoid() {
+		return infoid;
+	}
+
+	public void setInfoid(int infoid) {
+		this.infoid = infoid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public String getIntroduction() {
+		return introduction;
+	}
+
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+
+	@Override
+	public String toString() {
+		return "CustomerInfo [infoid=" + infoid + ", username=" + username + ", address=" + address + ", avatar="
+				+ avatar + ", introduction=" + introduction + "]";
+	}
 	
 	
 }
---
+++ src/main/java/neu/xiong/ELK/design/entity/Goods.java
@@ -0,0 +1,61 @@
+package neu.xiong.ELK.design.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="goods")
+public class Goods {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int goodsId;
+	
+	private String goodsName;
+	
+	private String goodsInfo;
+	
+	private String goodsType;
+
+	public int getGoodsId() {
+		return goodsId;
+	}
+
+	public void setGoodsId(int goodsId) {
+		this.goodsId = goodsId;
+	}
+
+	public String getGoodsName() {
+		return goodsName;
+	}
+
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+
+	public String getGoodsInfo() {
+		return goodsInfo;
+	}
+
+	public void setGoodsInfo(String goodsInfo) {
+		this.goodsInfo = goodsInfo;
+	}
+
+	public String getGoodsType() {
+		return goodsType;
+	}
+
+	public void setGoodsType(String goodsType) {
+		this.goodsType = goodsType;
+	}
+
+	@Override
+	public String toString() {
+		return "Goods [goodsId=" + goodsId + ", goodsName=" + goodsName + ", goodsInfo=" + goodsInfo + ", goodsType="
+				+ goodsType + "]";
+	}
+	
+	
+}
